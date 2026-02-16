233c3b829693f2c6a29657387139033a1a662045
Shoban Chiddarth <messageshoban@gmail.com>
2026-02-16 20:07:08 +0530

    Added remove indent option to vbox script

--- virtualbox/virtualbox-base.py
+++ virtualbox/virtualbox-base.py
@@ -1,10 +1,17 @@
+import os
 import subprocess
 
 def type_in_vm(string, VMname="Mint"):
     subprocess.run(["VBoxManage", "controlvm", VMname,'keyboardputstring', string])
 
-def get_content(filepath=".content.txt"):
+def get_content(filepath=".content.txt", remove_indent=bool(int(os.environ.get("REMOVE_INDENT")))):
+    data = []
     with open(filepath, "r") as fh:
-        return fh.read().strip()
+        data[:] = fh.readlines()
+    
+    if remove_indent:
+        data[:] = [i.lstrip() for i in data]
+    
+    return "".join(data)
 
 type_in_vm(get_content())
